ccb965bbf842ccd80703d77cded6a287d5fdd465
2T2BACMAC <scyozgat@gmail.com>
2025-04-26 10:22:31 +0300

    SleepCalculatorAdded_260425

--- LPE.py
+++ LPE.py
@@ -3,7 +3,8 @@
 def main_menu():
     projects = {
         'LPE1': 'Day Converter',
-        'LPE2': 'Kilograms to Pounds'
+        'LPE2': 'Kilograms to Pounds',
+        'LPE3': 'Sleep Calculator'
     }
 
     while True:
@@ -25,6 +26,8 @@ def main_menu():
                 day_converter()
             elif mainSelection == 'LPE2':
                 kg_converter()
+            elif mainSelection == 'LPE3':
+                sleep_calculator()
         else:
             print("You typed an undefined project name, try again.")
 
@@ -92,6 +95,83 @@ def kg_converter():
             else:
                 print("You typed a wrong key. Please type 'N' or 'Q'")
 
+
+def sleep_calculator():
+    while True:
+        minutes = 60
+
+        try:
+            print("--------Sleep Calculator App---------")
+            print("When did you hit the bed? (Hour, 0-23)")
+            bedHourInput = input("> ")
+
+            if not bedHourInput.isdigit() or len(bedHourInput) > 2:
+                print("Error: Please enter a valid hour (0-23) with max 2 digits!")
+                continue
+            bedHour = int(bedHourInput)
+            if bedHour > 23:
+                print("Error: Hour must be between 0-23!")
+                continue
+
+            print("Minutes? (0-59)")
+            bedMinInput = input("> ")
+
+            if not bedMinInput.isdigit() or len(bedMinInput) > 2:
+                print("Error: Please enter valid minutes (0-59) with max 2 digits!")
+                continue
+            bedMinutes = int(bedMinInput)
+            if bedMinutes > 59:
+                print("Error: Minutes must be between 0-59!")
+                continue
+
+            print("When did you wake up? (Hour, 0-23)")
+            wakeHourInput = input("> ")
+
+            if not wakeHourInput.isdigit() or len(wakeHourInput) > 2:
+                print("Error: Please enter a valid hour (0-23) with max 2 digits!")
+                continue
+            wakeHour = int(wakeHourInput)
+            if wakeHour > 23:
+                print("Error: Hour must be between 0-23!")
+                continue
+
+            print("Minutes? (0-59)")
+            wakeMinInput = input("> ")
+
+            if not wakeMinInput.isdigit() or len(wakeMinInput) > 2:
+                print("Error: Please enter valid minutes (0-59) with max 2 digits!")
+                continue
+            wakeMinutes = int(wakeMinInput)
+            if wakeMinutes > 59:
+                print("Error: Minutes must be between 0-59!")
+                continue
+
+        except ValueError:
+            print("Please enter numbers only!")
+            continue
+
+        bedTotal = bedHour * minutes + bedMinutes
+        wakeTotal = wakeHour * minutes + wakeMinutes
+
+        if wakeTotal < bedTotal:
+            wakeTotal += 24 * minutes  
+
+        totalSleep = wakeTotal - bedTotal
+        sleepHour = totalSleep // minutes
+        sleepMin = totalSleep % minutes
+
+        print(f"\nYou slept for {int(sleepHour)} hour(s) and {sleepMin} minute(s).\n")
+
+        while True:
+            print("Type 'N' for a new calculation or 'Q' for main menu.")
+            userInput = input("> ").upper()
+
+            if userInput == 'N':
+                break  
+            elif userInput == 'Q':
+                return  
+            else:
+                print("You typed a wrong key. Please type 'N' or 'Q'.")
 
 
 main_menu()
